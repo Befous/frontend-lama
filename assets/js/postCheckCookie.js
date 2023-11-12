424c812e30144f19f1128f5668a7546c1d7426eb
Befous <befousmain@gmail.com>
2023-11-12 18:51:21 +0700

    ggg

--- assets/js/postCheckCookie.js
+++ assets/js/postCheckCookie.js
@@ -3,9 +3,10 @@ import { getCookie} from "./cookie.js";
 export default function checkCookie() {
     let user = getCookie("token");
     let username = getCookie("username");
-	if (user != "" || user == null || user == undefined) {
+	if (user != "") {
 	alert("Welcome " + username);
 	} else {
+		alert("Biasakan login sebelum melakukan sesuatu");
 		window.location.href="../login";
 	}
 }
